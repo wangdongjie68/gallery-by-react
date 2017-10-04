606caa579475da20101f2e87efc1f05c13d41446
wangdongjie <wdj68@sina.com>
2017-10-04 13:21:09 +0800

    create stage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,38 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
+
 
 import React from 'react';
+
+//获取图片相关的数据
+var imageDatas = require('../data/imageDatas.json');
+
+//利用自执行函数，将图片名信息转成图片URL路径信息
+imageDatas = (function genImageURL(imageDatasArr){
+  for(var i = 0, j = imageDatasArr.length; i < j; i++){
+    var singleImageData = imageDatasArr[i];
+
+    singleImageData.imageURL = require('../images/' + singleImageData.filename);
+  }
+
+  return imageDatasArr;
+})(imageDatas);
 
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
+      // <div className="index">
+      //   <img src={yeomanImage} alt="Yeoman Generator" />
+      //   <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      // </div>
     );
   }
 }
